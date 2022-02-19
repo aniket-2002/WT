54ec3535de5171382de60af312bf36d83e4438b1
aniket14 <ac745273@gmail.com>
2022-02-19 18:58:05 +0530

    commit

---
+++ Lab 3/Lab3q1.java
@@ -0,0 +1,9 @@
+public class Lab3q1{
+    public static void main(String args[]){
+        System.out.println("Name: Aniket Chatterjee");
+        System.out.println("Class: CSE26");
+        System.out.println("RollNo: 2005783");
+        System.out.println("Marks: "+ 100);
+        System.out.println("Age: "+ 19);
+    }
+}
---
+++ Lab 3/Lab3q4.java
@@ -0,0 +1,6 @@
+public class Lab3q4 {
+    public static void main(String args[]){
+        int randarray = (int)(10*Math.random()%6) +1;
+        System.out.println(randarray);
+    }
+}
